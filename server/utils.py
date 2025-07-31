13de4bf0d9b0978e74867608c1aabd9741aebbf8
Naveen Sriram <62332925+NaveenSriramR@users.noreply.github.com>
2025-07-31 03:40:57 +0100

    util files

---
+++ server/utils.py
@@ -0,0 +1,15 @@
+import httpx
+
+async def reverse_geocode(lat: float, lng: float) -> str:
+    url = "https://nominatim.openstreetmap.org/reverse"
+    params = {
+        "lat": lat,
+        "lon": lng,
+        "format": "json"
+    }
+    headers = {"User-Agent": "fastapi-reverse-geocoder"}
+
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url, params=params, headers=headers)
+        data = resp.json()
+        return data.get("display_name", "Unknown address")
